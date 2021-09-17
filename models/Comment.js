3242342a6673f9754611221db407a16e49c704cf
Anthony Pena <apena5@alumni.jh.edu>
2021-09-17 17:42:13 -0400

    user dashboard finished

--- models/Comment.js
+++ models/Comment.js
@@ -8,16 +8,19 @@ Comment.init(
         // columns will go here
         id: {
             type: DataTypes.INTEGER,
+            allowNull: false,
             primaryKey: true,
             autoIncrement: true
         },
         comment_text: {
             type: DataTypes.STRING,
-            allowNull: false
+            allowNull: false,
+            validate: {
+                len: [1]
+            }
         },
         user_id: {
             type: DataTypes.INTEGER,
-            allowNull: false,
             references: {
                 model: 'user',
                 key: 'id'
@@ -25,7 +28,6 @@ Comment.init(
         },
         post_id: {
             type: DataTypes.INTEGER,
-            allowNull: false,
             references: {
                 model: 'post',
                 key: 'id'
